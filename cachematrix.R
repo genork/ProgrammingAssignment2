8ed5361fd637483d3f97803f49ae808640d53f4c
GenoRK <gruckm@gmail.com>
2015-01-25 18:57:43 -0500

    Completed cachematrix.R
    Completed for Assignment inverse Matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions are for the R-Programming Assignment 2: Lexical Scoping
+## Credit for the code logic goes to course instructor Roger D. Peng, PhD
+## because he provided an example to cache  a mean value and return it - 
+## I just changed his code logic to cache an inverse of a matrix through the
+## following two functions
 
-## Write a short comment describing this function
+## makeCacheMatrix is a function that creates a special "matrix" object 
+## that can cache its inverse by
+## 1. set the value of the matrix
+## 2. get the value of the matrix
+## 3. set the value of the inverse of the matrix
+## 4. get the value of the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+        
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmatrix <- function(solve) m <<- solve
+        getmatrix <- function() m
+        list(set = set, get = get,
+             setmatrix = setmatrix,
+             getmatrix = getmatrix)
 
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by 
+## makeCacheMatrix above. If the inverse has already been calculated 
+## (and the matrix has not changed), then the cachesolve should retrieve 
+## the inverse from the cache.
+## For this assignment, assume that the matrix supplied is always invertibl
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
+        m <- x$getmatrix()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setmatrix(m)
+        m
+        
 }
